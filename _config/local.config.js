13b0a09944a74a3f0fd6f1c4d542bbcf0c629b71
XtheEXILEmercyX <jarnevalkeneers7@gmail.com>
2020-08-15 21:36:08 +0200

    index

--- _config/local.config.js
+++ _config/local.config.js
@@ -1,7 +1,7 @@
 const config = {
     name: "AfterShock",
     prefix: "a!",
-    token: 'NzA5NTE5MjkxMTQxMzI0OTAy.XrnFVQ.4Gcrziu_s1jQBgMDEPxUSj2hDv8',
+    token: 'NzA5NTE5MjkxMTQxMzI0OTAy.XrnFVQ.36ZL7iZt3VvPzSHhUmua-dGfGcg',
     developers: ['316639200462241792', '395980281695305729']
 };
 

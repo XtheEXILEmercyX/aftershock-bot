d66237b87388daeeccabfae279f01a07fc7df0d7
NoxFly <dorian.thivolle@gmail.com>
2020-08-16 17:17:10 +0200

    create commands command

--- commands/commands.js
+++ commands/commands.js
@@ -2,14 +2,34 @@ const Discord = require('discord.js');
 
 module.exports = {
     execute(client, message, args) {
-        // array of commands name
-        const commands = Array.from(client.commands.keys());
-
+        
         const embed = new Discord.MessageEmbed()
             .setColor(client.mainColor)
-            .setTitle("Commands")
-            .setDescription(`do \`${client.prefix}commands commandName\` to have the description of a command.\n\n` + commands.map(c => '`' + c + '`').join(', '));
+            .setTitle("Commands");
+
+        if(args.length === 0) {
+            // array of commands name
+            const commands = Array.from(client.commands.keys());
+
+            embed.setDescription(`do \`${client.prefix}commands commandName\` to have the description of a command.\n\n` + commands.map(c => '`' + c + '`').join(', '));
+
+        }
+
+        else {
+            const cmd = args[0];
+
+            embed.addField('Command :', cmd);
+
+            if(!client.commands.has(cmd)) {
+                embed.setDescription("Command not found");
+            }
+
+            else {
+                embed.addField('Description', client.commands.get(cmd).description);
+            }
+        }
 
         message.channel.send(embed);
+
     }
 }
